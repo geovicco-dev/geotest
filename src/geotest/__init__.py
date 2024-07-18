f3ff8309280fb0014e98b9ffed6d0b2d3610db53
geovicco-dev <geovicco.dev@gmail.com>
2024-07-18 15:40:19 +1000

    added project.scripts in pyproject.toml and updated cli app with new commands

--- src/geotest/__init__.py
+++ src/geotest/__init__.py
@@ -1,10 +1,18 @@
 import typer
+from datetime import date
 
 app = typer.Typer()
 
 @app.command()
-def hello() -> str:
-    return "Hello from geotest!"
+def hello(name: str = typer.Option(default="World", help="Name to greet")) -> str:
+    typer.echo(f"Hello {name}!")
+
+def ask_age() -> int:
+    return typer.prompt("How old are you?")
+
+@app.command()
+def year_born(age: int = typer.Argument(ask_age)):
+    typer.echo(f"You were born in {date.today().year - age}")
 
 def main():
     app()
